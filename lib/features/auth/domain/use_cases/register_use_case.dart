ae44bc816cf0cb08838ff9ccad009c4f966c0f3e
JhosepAC <U20231D978@upc.edu.pe>
2025-11-28 12:13:37 -0500

    chore: format register_use_case.dart

--- lib/features/auth/domain/use_cases/register_use_case.dart
+++ lib/features/auth/domain/use_cases/register_use_case.dart
@@ -1,11 +1,17 @@
 import '../entities/user_entity.dart';
 import '../repositories/auth_repository.dart';
 
+/// Use case responsible for handling user registration.
+///
+/// Delegates the creation of a new user to the provided [AuthRepository].
 class RegisterUseCase {
   final AuthRepository repository;
 
   RegisterUseCase(this.repository);
 
+  /// Executes the registration process for the given [user].
+  ///
+  /// Returns the created [UserEntity] after successful registration.
   Future<UserEntity> execute(UserEntity user) {
     return repository.register(user);
   }
